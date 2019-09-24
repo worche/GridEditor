f5742c4ae017872995cba62c2d728c5472fb4136
Mehmet Can CİCEK <m.c.cicek@hotmail.com>
2019-09-24 18:52:23 +0300

    ColorPicker

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChange : MonoBehaviour
+{
+
+    Button btn;
+    ColorManager instance;
+    [SerializeField] Image PreviousColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = ColorManager.instance;
+        btn =GetComponent<Button>();
+        btn.onClick.AddListener(Click);
+    }
+
+    void Click()
+    {
+        instance.NextColor();
+        PreviousColor.color = GetComponent<Image>().color;;
+        GetComponent<Image>().color = instance.GetCurrentColor();
+    }
+}
---
+++ Assets/ColorManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    public static ColorManager instance;
+    Queue<Color> colorQueue;
+
+    Color defaultColor;
+    [ColorUsageAttribute(true, false)] [SerializeField] Color color1;
+    [ColorUsageAttribute(true, false)] [SerializeField] Color color2;
+    [ColorUsageAttribute(true, false)] [SerializeField] Color color3;
+
+    Color selecterColor;
+    #region Singeleton
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    private void Start()
+    {
+        colorQueue = new Queue<Color>();
+        defaultColor = Color.white;
+        selecterColor = defaultColor;
+        colorQueue.Enqueue(color1);
+        colorQueue.Enqueue(color2);
+        colorQueue.Enqueue(color3);
+    }
+
+    public Color GetCurrentColor()
+    {
+        if (selecterColor == null)
+            return defaultColor;
+
+        return selecterColor;
+    }
+    public void NextColor()
+    {
+        selecterColor = colorQueue.Dequeue();
+        colorQueue.Enqueue(selecterColor);
+    }
+}
